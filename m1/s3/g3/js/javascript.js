9697ff1d61eaa850cc2a48999d808e5b714258fe
SimoD91 <simone0091@hotmail.com>
2023-10-11 16:53:12 +0200

    esercizio s3/g3

---
+++ m1/s3/g3/js/javascript.js
@@ -0,0 +1,57 @@
+/* 
+È giusto dirti che non è roba mia, ho trovato questi su w3school cercando su google ma li utilizzo principalmente 
+per rileggerli a voce alta e capirne il meccanismo in relazione a css e html con qualche modifica (principalmente html e css,
+ho provato a fare mille modifiche anche con js ma spaccavo tutto in continuazione).
+*/
+
+let lista = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < lista.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  lista[i].appendChild(span);
+}
+
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("myInput").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Campo vuoto");
+} else {
+    document.getElementById("myUL").appendChild(li);
+  }
+  document.getElementById("myInput").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function() {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
