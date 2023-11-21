d81d9722a3807b37e256370ff3ef384f5a858646
SimoD91 <simone0091@hotmail.com>
2023-11-21 16:57:34 +0100

    esercizio m3/s1/g2 (Typescript - Classi)

---
+++ m3/s1/g2/script.ts
@@ -0,0 +1,32 @@
+class SonAccount {
+    balanceInit: number = 0;
+    oneDeposit(amount: number): void {
+        this.balanceInit += amount;
+        console.log("Saldo depositato", this.balanceInit);
+    }
+    oneWithDraw(amount: number): void {
+        if (amount <= this.balanceInit) {
+            this.balanceInit -= amount;
+            console.log("Prelievo avvenuto con successo");
+        }else{
+            console.log("Saldo non disponibile", amount);
+}
+}
+}
+
+class MotherAccount extends SonAccount {
+    addInterest(): void {
+        let interest: number = this.balanceInit * 0.1;
+        this.balanceInit += interest;
+}
+}
+
+let sonAccount = new SonAccount();
+sonAccount.oneDeposit(300);
+sonAccount.oneWithDraw(270);
+
+let motherAccount = new MotherAccount();
+motherAccount.oneDeposit(290);
+motherAccount.oneWithDraw(80);
+motherAccount.addInterest();
+console.log("Credito", motherAccount.balanceInit);
